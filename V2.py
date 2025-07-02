aeee79efb54e65f48aec73fcf9c24ff055897ae2
Chris Smith <christopher.smith2005@outlook.com>
2025-07-01 20:59:02 -0400

    Complete

---
+++ V2.py
@@ -0,0 +1,22 @@
+
+def main():
+
+
+    try:
+        user_input = input("Enter the numbers youd like to use for the pattern")
+
+        with open("pixel_pattern.txt", "w") as file:
+            file.write(user_input)
+
+
+        with open("pixel_pattern.txt", "r") as file:
+                  line = file.readline()
+                  pattern = line.strip().split(",")
+                  pattern = [int(x) for x in pattern]
+                  print(pattern)
+
+    except IOError:
+        print("Error, please try again")
+
+if __name__ == "__main__":
+    main()
